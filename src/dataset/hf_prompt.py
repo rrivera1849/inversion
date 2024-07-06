81dfae9a1ef397ec18f9726c38fa2dd66a3bb9f2
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2024-07-06 08:02:27 -0700

    updates to HF prompting script, includes LLAMA-3 and fallback mechanisms

--- src/dataset/hf_prompt.py
+++ src/dataset/hf_prompt.py
@@ -18,7 +18,8 @@ parser.add_argument("--dirname", type=str,
                     default="/data1/yubnub/changepoint/s2orc_changepoint/unit_128",
                     help="Directory where the dataset is stored.")
 parser.add_argument("--model_name", type=str, default="google/gemma-7b-it",
-                    choices=["google/gemma-7b-it", "mistralai/Mistral-7B-Instruct-v0.3", "mistralai/Mixtral-8x7B-Instruct-v0.1"],
+                    choices=["google/gemma-7b-it", "mistralai/Mistral-7B-Instruct-v0.3", 
+                             "mistralai/Mixtral-8x7B-Instruct-v0.1", "meta-llama/Meta-Llama-3-8B-Instruct"],
                     help="Huggingface model name to use for generation.")
 parser.add_argument("--split", type=str, default="validation",
                     help="Dataset split to generate prompts for.")
@@ -35,10 +36,15 @@ parser.add_argument("--temperature", type=float, default=0.7,
                     help="Generation temperature.")
 parser.add_argument("--top_p", type=float, default=0.9,
                     help="Generation top-p.")
+parser.add_argument("--fall_back", default=False, action="store_true",
+                    help="Fall back to smaller batch size if the number of tokens is too large.")
+parser.add_argument("--fall_back_num_tokens", type=int, default=450,
+                    help="Number of tokens to fall back to.")
 parser.add_argument("--debug", action="store_true", 
                     help="Enable debugging model.")
 args = parser.parse_args()
 
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 os.environ["TIKTOKEN_CACHE_DIR"] = "/tmp/riverasoto1"
 
 DIRNAME = args.dirname
@@ -60,6 +66,10 @@ elif "Mixtral" in args.model_name.split('/')[1]:
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     tokenizer.pad_token = tokenizer.eos_token
     model.generation_config.pad_token_id = tokenizer.pad_token_id
+elif "Meta-Llama" in args.model_name.split('/')[1]:
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, padding_side="left")
+    tokenizer.pad_token = tokenizer.eos_token
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
 else:
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
@@ -70,23 +80,39 @@ generation_config = GenerationConfig(
     top_p=args.top_p,
 )
 
-def get_generations(prompts):
+def count_num_tokens(prompt):
+    return len(tokenizer(prompt)["input_ids"])
+
+def get_generations(prompts, sub_batch_size=1):
     """Returns a list of completions for the given prompts.
     """
-    max_length = 2048 if args.prompt == "rephrase_with_context" else 1024
-    inputs = tokenizer(
-        prompts, 
-        max_length=max_length, 
-        padding=True, 
-        truncation=True,
-        return_tensors="pt", 
-    ).to("cuda")
-    
-    output = model.generate(**inputs, generation_config=generation_config)
+    with torch.inference_mode():
+        max_length = 2048 if args.prompt == "rephrase_with_context" else 1024
 
-    generations = [
-        tokenizer.decode(output[i], skip_special_tokens=True)[len(prompts[i]):] for i in range(len(prompts))
-    ]
+        generations = []
+        num_tokens = [count_num_tokens(prompt) for prompt in prompts]
+
+        if args.fall_back and max(num_tokens) > args.fall_back_num_tokens:
+            print("Falling to smaller batch size due to large number of tokens...")
+            prompts = [prompts[i:i+sub_batch_size] for i in range(0, len(prompts), sub_batch_size)]
+        else:
+            prompts = [prompts]
+
+        for j, prompt_batch in enumerate(prompts):
+            inputs = tokenizer(
+                prompt_batch,
+                max_length=max_length, 
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+            ).to("cuda")
+            output = model.generate(**inputs, generation_config=generation_config)
+            generations.extend([
+                tokenizer.decode(output[i], skip_special_tokens=True)[len(prompt_batch[i]):] 
+                for i in range(len(prompt_batch))
+            ])
+
+    assert len(generations) == sum(len(prompt_batch) for prompt_batch in prompts)
     return generations
 
 dataset_name = "{}_{}_prompt={}_temperature={}_top_p={}.jsonl".format(
