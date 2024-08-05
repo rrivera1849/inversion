ab62544ccc2b03b58365a752a49c78b61c5ad86d
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2024-08-05 11:41:12 -0700

    dataset creation script for mixture model now handles all the RAID domains

--- src/baselines/mixture/create_mixture_dataset.py
+++ src/baselines/mixture/create_mixture_dataset.py
@@ -1,9 +1,15 @@
+"""
+TODO
+1. Handle the case where we want a mix of domains (not all).
+2. Make sure this works with other tokenizers.
+"""
 
 import json
 import os
 import random
 import sys
 from argparse import ArgumentParser
+from collections import Counter
 
 import Levenshtein
 from datasets import load_from_disk
@@ -13,20 +19,27 @@ from tqdm import tqdm
 random.seed(43)
 
 parser = ArgumentParser()
-parser.add_argument("--dirname", type=str,
-                    default="/data1/yubnub/changepoint/s2orc_changepoint/unit_128",
-                    help="Directory where the dataset is stored.")
-parser.add_argument("--num_samples", type=int, default=100_000,
-                    help="Number of samples to draw from the dataset in total.")
-parser.add_argument("--units_perc_per_sample", type=float, default=0.10,
-                    help="Percentage of units to take from each paper when creating the dataset.")
+parser.add_argument("--max_num_samples", type=int, default=250_000,
+                    help="Maximum number of samples to use for the dataset.")
+parser.add_argument("--stratified", default=False, action="store_true",
+                    help="Whether to sample the dataset stratified by domain.")
 parser.add_argument("--tokenizer", type=str, default="roberta-large",
                     help="Tokenizer to use for the dataset.")
+parser.add_argument("--domain", type=str, default="all",
+                    choices=["all", "books", "news", "wiki", "reddit", "recipes", "poetry", "abstracts", "reviews", "s2orc"],
+                    help="Domains to use when creating the dataset.")
 parser.add_argument("--debug", default=False, action="store_true")
 args = parser.parse_args()
 
-def get_levenshtein_tags(string1, string2, tokenizer):
-    """Code taken from: https://github.com/machelreid/lewis/blob/master/roberta-tagger/preprocess-roberta-tagger.py
+DATASET_ELEM_TYPE = tuple[str, str, tuple[int, list[int]]]
+
+def get_levenshtein_tags(
+    string1: str, 
+    string2: str, 
+    tokenizer: AutoTokenizer,
+):
+    """Code taken and adapted from: 
+        https://github.com/machelreid/lewis/blob/master/roberta-tagger/preprocess-roberta-tagger.py
 
        TODO: Comment the function and cleanup
     """
@@ -77,41 +90,47 @@ def get_levenshtein_tags(string1, string2, tokenizer):
         former_key = i
     mask2fill = mask2fill
     return output_list
+    
+def get_text_subset(text: str, tokenizer: AutoTokenizer, max_length: int = 510):
+    return tokenizer.decode(tokenizer(text)["input_ids"][1:-1][:max_length], skip_special_tokens=True)
 
-def main():
-    tokenizer_name = args.tokenizer.split("/")[-1]
-    fname = f"./mixture_dataset_tokenizer={tokenizer_name}_num-samples={args.num_samples}.jsonl"
-    if os.path.isfile(fname):
-        print(f"{fname}, already exists...")
-        return 0
+def read_data(
+    tokenizer: AutoTokenizer,
+    from_s2orc: bool = False,
+) -> DATASET_ELEM_TYPE:
+    # Returns: (text, domain, (label, tagger_labels))
 
-    dataset = load_from_disk(os.path.join(args.dirname, "train_clean_and_joined"))
-
+    if from_s2orc:
+        dirname = "/data1/yubnub/changepoint/s2orc_changepoint/unit_128/train_clean_and_joined"
+    else:
+        dirname = "/data1/yubnub/changepoint/RAID_rephrase/train_human_unit_128_clean_and_joined"
+    
+    dataset = load_from_disk(dirname)
     if args.debug:
         dataset = dataset.select(range(1_000))
-        
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-    if tokenizer.pad_token is None:
-        tokenizer.pad_token = tokenizer.eos_token
-    
-    def get_text_subset(text: str, tokenizer: AutoTokenizer, max_length: int = 510):
-        return tokenizer.decode(tokenizer(text)["input_ids"][1:-1][:max_length], skip_special_tokens=True)
-    
+
     dataset_positive_samples = []
     dataset_negative_samples = []
     
     for i in tqdm(range(len(dataset))):
+        if not from_s2orc and args.domain != "all" and dataset[i]["domain"] != args.domain:
+            continue
+        domain = "s2orc" if from_s2orc else dataset[i]["domain"]
 
         sample = dataset[i]
         original_text = sample["units"]
         
         # Sample the Negative (human) samples:
-        K = int(args.units_perc_per_sample * len(original_text))
-        indices_to_sample = random.sample(range(len(original_text)), k=K)
+        if from_s2orc:
+            K = int(0.10 * len(original_text))
+            indices_to_sample = random.sample(range(len(original_text)), k=K)
+        else:
+            indices_to_sample = range(len(original_text))
+            
         for index in indices_to_sample:
             text = get_text_subset(original_text[index], tokenizer)
             dataset_negative_samples.append((
-                text,
+                text, domain,
                 (0, [0] * len(tokenizer.tokenize(text)))
             ))
         
@@ -123,8 +142,11 @@ def main():
             generations = sample[gkey]
             changepoint_indices = sample[ckey]
             
-            K = int(args.units_perc_per_sample * len(generations))
-            indices_to_sample = random.sample(range(len(generations)), k=K)
+            if from_s2orc:
+                K = int(0.10 * len(generations))
+                indices_to_sample = random.sample(range(len(generations)), k=K)
+            else:
+                indices_to_sample = range(len(generations))
 
             for index in indices_to_sample:
                 generation = generations[index]
@@ -136,28 +158,97 @@ def main():
 
                 text = get_text_subset(generation, tokenizer)
                 dataset_positive_samples.append((
-                    text,
+                    text, domain,
                     (1, tag_labels[:len(tokenizer.tokenize(text))])
                 ))
 
-        if len(dataset_negative_samples) >= args.num_samples and len(dataset_positive_samples) >= args.num_samples:
-            break
-
-    random.shuffle(dataset_negative_samples)
-    random.shuffle(dataset_positive_samples)
-    dataset_negative_samples = dataset_negative_samples[:args.num_samples]
-    dataset_positive_samples = dataset_positive_samples[:args.num_samples]
+    N = min(len(dataset_negative_samples), len(dataset_positive_samples))
+    dataset_negative_samples = random.sample(dataset_negative_samples, k=N)
+    dataset_positive_samples = random.sample(dataset_positive_samples, k=N)
     dataset_mixture = dataset_negative_samples + dataset_positive_samples
+
+    return dataset_mixture
+
+def create_dataset_dir() -> str:
+    dataset_dirname = f"./datasets/{args.domain}_{args.tokenizer}_{args.max_num_samples}"
+    if args.debug:
+        dataset_dirname += "_debug"
+        
+    os.makedirs(dataset_dirname, exist_ok=True)
+    if len(os.listdir(dataset_dirname)) > 0:
+        raise ValueError(f"Dataset directory {dataset_dirname} is not empty.")
     
-    with open(fname, "w+") as fout:
+    return dataset_dirname
+
+def save_dataset(
+    dataset_mixture: DATASET_ELEM_TYPE,
+    tokenizer: AutoTokenizer,
+    savename: str,
+):
+    with open(savename, "w+") as fout:
         for sample in dataset_mixture:
             record = {
                 "text": sample[0],
-                "label": sample[1][0],
-                "tagger_labels": sample[1][1],
+                "domain": sample[1],
+                "label": sample[-1][0],
+                "tagger_labels": sample[-1][1],
             }
             record.update(tokenizer(sample[0], max_length=512, truncation=True, padding="max_length"))
             fout.write(json.dumps(record)); fout.write('\n')
+
+def main():
+    dataset_dirname = create_dataset_dir()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    print("Reading data...")
+    dataset_mixture = []
+    if args.domain == "all" or args.domain == "s2orc":
+        dataset_mixture += read_data(tokenizer, from_s2orc=True)
+    if args.domain != "s2orc":
+        dataset_mixture += read_data(tokenizer, from_s2orc=False)
+    assert len(dataset_mixture) > 0
+    random.shuffle(dataset_mixture)
+    
+    if args.stratified:
+        print("Stratifying dataset...")
+        dataset_mixture_domain_counts = Counter(sample[1] for sample in dataset_mixture)
+        min_domain_count = min(dataset_mixture_domain_counts.values())
+        domain_counts = {domain: 0 for domain in dataset_mixture_domain_counts.keys()}
+        dataset_stratified = []
+        for sample in dataset_mixture:
+            domain = sample[1]
+            if domain_counts[domain] < min_domain_count:
+                dataset_stratified.append(sample)
+                domain_counts[domain] += 1
+                
+            if all([v == min_domain_count for v in domain_counts.values()]):
+                break
+            
+        dataset_mixture = dataset_stratified
+
+    dataset_mixture = dataset_mixture[:2 * args.max_num_samples]
+
+    print("Saving dataset...")
+    train_size = int(0.70 * len(dataset_mixture))
+    val_size = int(0.15 * len(dataset_mixture))
+    save_dataset(
+        dataset_mixture[:train_size],
+        tokenizer,
+        f"{dataset_dirname}/train.jsonl",
+    )
+    save_dataset(
+        dataset_mixture[train_size:train_size + val_size],
+        tokenizer,
+        f"{dataset_dirname}/valid.jsonl",
+    )
+    save_dataset(
+        dataset_mixture[train_size + val_size:],
+        tokenizer,
+        f"{dataset_dirname}/test.jsonl",
+    )
 
     return 0
 
