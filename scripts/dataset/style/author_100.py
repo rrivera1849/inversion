53bafc129f95145f215212e074f5cf8adf14e691
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2024-08-09 06:08:34 -0700

    author_id dataset mod

--- scripts/dataset/style/author_100.py
+++ scripts/dataset/style/author_100.py
@@ -12,6 +12,8 @@ random.seed(43)
 
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
+SUFFIX = ".politics"
+
 def split(row):
     # 15% for validation and 15% for test
     N = len(row["syms"])
@@ -29,7 +31,7 @@ def filter_by_lengths(row):
     N = len(row["syms"])
     indices_to_keep = []
     for i in range(N):
-        if row["num_tokens"][i] >= 128 and row["num_tokens"][i] <= 512:
+        if row["num_tokens"][i] >= 64 and row["num_tokens"][i] <= 512:
             indices_to_keep.append(i)
     for k, v in row.items():
         if isinstance(v, list):
@@ -38,19 +40,24 @@ def filter_by_lengths(row):
 
 def main():
     base_path = "/data1/yubnub/data/iur_dataset"
-    os.makedirs(os.path.join(base_path, "author_100"), exist_ok=True)
+    
+    dirname = "author_100" + SUFFIX
+    os.makedirs(os.path.join(base_path, dirname), exist_ok=True)
 
-    df = pd.read_json(os.path.join(base_path, "train.jsonl"), lines=True)
-
+    df = pd.read_json(os.path.join(base_path, "train.jsonl" + SUFFIX), lines=True)
+    df = df[df["syms"].apply(len) > 50]
+    
     df["average_text_len"] = df["lens"].apply(lambda x: sum(x) / len(x))
     df = df.sort_values("average_text_len", ascending=False)
-    df = df.head(1_000)
+    # df = df.head(1_000)
 
     df["num_tokens"] = df["syms"].progress_apply(lambda lst: [len(tokenizer.encode(s)) for s in lst])
     df = df.progress_apply(filter_by_lengths, axis=1)
     df = df[~(df["syms"].apply(len) == 0)]
     df = df[df["syms"].apply(len) >= 50]
     df = df.head(100)
+
+    import pdb; pdb.set_trace()
 
     df = df[["author_id", "syms"]]
     N = df["syms"].apply(len).min()
@@ -66,9 +73,9 @@ def main():
     train = train.rename(columns={"syms_train": "syms"})
     valid = valid.rename(columns={"syms_valid": "syms"})
     queries = queries.rename(columns={"syms_test": "syms"})
-    train.to_json(os.path.join(base_path, "author_100", "train.jsonl"), lines=True, orient="records")
-    valid.to_json(os.path.join(base_path, "author_100", "valid.jsonl"), lines=True, orient="records")
-    queries.to_json(os.path.join(base_path, "author_100", "test.jsonl"), lines=True, orient="records")
+    train.to_json(os.path.join(base_path, dirname, "train.jsonl"), lines=True, orient="records")
+    valid.to_json(os.path.join(base_path, dirname, "valid.jsonl"), lines=True, orient="records")
+    queries.to_json(os.path.join(base_path, dirname, "test.jsonl"), lines=True, orient="records")
 
 if __name__ == "__main__":
     sys.exit(main())
