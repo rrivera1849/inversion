eedb97f8c92e684f5ce4465d20cf4a31922bb795
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2024-08-11 13:07:39 -0700

    different aggregation methods and evaluating on all test sets

--- src/baselines/mixture/downstream/author_classification.py
+++ src/baselines/mixture/downstream/author_classification.py
@@ -1,4 +1,17 @@
+"""
+To Implement:
+1. Aggregation Methods
+    - Learned Aggregation
+        - With / Without Mixture Predictor
+    - <human> and <machine> embeddings
+2. Save Model, Evaluate on Multiple Test Sets
 
+Modes:
+- Human / Mixed / LLM
+    - With / Without Mixture Predictor
+    
+- On -> Human / Mixed / LLM with Mixture Predictor if Mixture Predictor was used
+"""
 import json
 import os
 import sys
@@ -18,17 +31,24 @@ sys.path.append("../")
 
 parser = ArgumentParser()
 parser.add_argument("--train_suffix", type=str, default="")
-parser.add_argument("--test_suffix", type=str, default="")
 parser.add_argument("--num_epoch", type=int, default=20)
 parser.add_argument("--batch_size", type=int, default=16)
 parser.add_argument("--learning_rate", type=float, default=5e-5)
+parser.add_argument("--method", type=str, default="fixed_weight", nargs="+",
+                    choices=["fixed_weight", "learned_weight", "learned_weight_no_bias", "mixture_embeddings"],
+                    help="Methods to use when incorporating Mixture Predictor weights.")
 parser.add_argument("--token_mixture_multiplier", type=float, default=1.0)
 args = parser.parse_args()
 
 DATA_PATH = "/data1/yubnub/data/iur_dataset/author_100.politics"
 
-def mean_pooling(model_output, attention_mask, weights=None):
-    token_embeddings = model_output.last_hidden_state
+def mean_pooling(
+    token_embeddings: torch.Tensor, 
+    attention_mask: torch.Tensor, 
+    weights: torch.Tensor = None,
+):
+    """Mean pooling with optional weights.
+    """
     if weights is not None:
         token_embeddings = token_embeddings * weights.unsqueeze(-1).expand(token_embeddings.size())
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size())
@@ -37,26 +57,62 @@ def mean_pooling(model_output, attention_mask, weights=None):
     return sum_embeddings / sum_mask
 
 class Classifier(nn.Module):
+    """Simple RoBERTa-based classifier.
+        RoBERTa -> Mean Pooling -> Linear
+    """
     def __init__(self):
         super().__init__()
         self.model = AutoModel.from_pretrained("roberta-base", add_pooling_layer=False)
         self.classifier = nn.Linear(self.model.config.hidden_size, 100)
         self.loss = nn.CrossEntropyLoss()
+
+        if "learned_weight" in args.method or "learned_weight_no_bias" in args.method:
+            self.token_weight_predictor = nn.Linear(self.model.config.hidden_size, 1)
+
+        if "mixture_embeddings" in args.method:
+            # <human> and <machine> embeddings:
+            self.mixture_embeddings = nn.Embedding(2, self.model.config.hidden_size)
     
     def forward(
         self, 
         inputs: dict
     ) -> dict:
         
+        input_embeddings = self.model.embeddings.word_embeddings(inputs["input_ids"])
+        if "mixture_embeddings" in args.method:
+            assert "human_probs" in inputs and "machine_probs" in inputs
+            human_embeddings = self.mixture_embeddings(torch.zeros_like(inputs["input_ids"]))
+            machine_embeddings = self.mixture_embeddings(torch.ones_like(inputs["input_ids"]))
+            input_embeddings = input_embeddings + \
+                human_embeddings * inputs["human_probs"].unsqueeze(-1).expand(input_embeddings.size()) + \
+                    machine_embeddings * inputs["machine_probs"].unsqueeze(-1).expand(input_embeddings.size())
+
         out = self.model(
-            input_ids=inputs["input_ids"],
+            inputs_embeds=input_embeddings,
             attention_mask=inputs["attention_mask"],
         )
-        if "token_mixture_preds" in inputs:
-            token_mixture_preds = inputs["token_mixture_preds"]
-            out = mean_pooling(out, inputs["attention_mask"], weights=token_mixture_preds)
+
+        if "learned_weight" in args.method or "learned_weight_no_bias" in args.method:
+            lengths = (inputs["attention_mask"].sum(1) - 2)
+            weights = []
+            for j, length in enumerate(lengths):
+                token_weights = self.token_weight_predictor(out["last_hidden_state"][j, 1:length+1]).squeeze()
+                token_weights = torch.nn.functional.softmax(token_weights, dim=0)
+                weight = torch.zeros_like(inputs["attention_mask"][j]).type_as(token_weights)
+                weight[1:length+1] = token_weights
+                weights.append(weight)
+            weights = torch.stack(weights)
+            if "learned_weight_no_bias" in args.method:
+                weights = 1. + args.token_mixture_multiplier * weights
+            else:
+                weights = 1. + args.token_mixture_multiplier * (weights + inputs["human_probs"])
+        elif "fixed_weight" in args.method:
+            weights = 1. + args.token_mixture_multiplier * inputs["human_probs"]
         else:
-            out = mean_pooling(out, inputs["attention_mask"])
+            weights = None
+
+        out = mean_pooling(out["last_hidden_state"], inputs["attention_mask"], weights=weights)
+
         out = self.classifier(out)
         loss = self.loss(out, inputs["label"].squeeze())
 
@@ -98,12 +154,13 @@ class JSONLDataset(Dataset):
         inputs["label"] = torch.LongTensor([row["label"]])
 
         if "token_mixture_preds" in row:
-            token_mixture_preds = row["token_mixture_preds"]
-            token_mixture_preds = [1. + pred[0] * args.token_mixture_multiplier for pred in token_mixture_preds]
-            token_mixture_preds = [0.] + token_mixture_preds
-            token_mixture_preds += [0.] * (512 - len(token_mixture_preds))
-            token_mixture_preds = torch.FloatTensor(token_mixture_preds)
-            inputs["token_mixture_preds"] = token_mixture_preds
+            inputs["human_probs"] = [pred[0] for pred in row["token_mixture_preds"]]
+            inputs["human_probs"] = [0.] + inputs["human_probs"] + [0.] * (512 - len(inputs["human_probs"]) - 1)
+            inputs["human_probs"] = torch.FloatTensor(inputs["human_probs"])
+
+            inputs["machine_probs"] = [pred[1] for pred in row["token_mixture_preds"]]
+            inputs["machine_probs"] = [0.] + inputs["machine_probs"] + [0.] * (512 - len(inputs["machine_probs"]) - 1)
+            inputs["machine_probs"] = torch.FloatTensor(inputs["machine_probs"])
 
         return inputs
     
@@ -123,18 +180,6 @@ def get_dataset_distribution_name(name: str):
         return "mistral"
     elif name.endswith(".mistral.mixed") or name.endswith(".mistral.mixed.token_mixture_preds"):
         return "mixed"
-    elif name.endswith("mistral.oracle"):
-        return "mistral_oracle"
-    elif name.endswith("mistral.mixed.oracle"):
-        return "mixed_oracle"
-    elif name.endswith("mistral.uniform"):
-        return "mistral_uniform"
-    elif name.endswith("mistral.mixed.uniform"):
-        return "mixed_uniform"
-    elif name.endswith(".oracle"):
-        return "human_oracle"
-    elif name.endswith(".uniform"):
-        return "human_uniform"
     elif name.endswith(".token_mixture_preds") or name == "":
         return "human" 
     else:
@@ -143,25 +188,21 @@ def get_dataset_distribution_name(name: str):
 def main():
     os.makedirs("./outputs/author_classification_100", exist_ok=True)
 
-    if args.train_suffix == "" and args.test_suffix.endswith("token_mixture_preds"):
-        raise ValueError(f"Invalid combination of train_suffix and test_suffix: {args.train_suffix}, {args.test_suffix}")
     train_fname = os.path.join(DATA_PATH, f"train.jsonl{args.train_suffix}")
     valid_fname = os.path.join(DATA_PATH, f"valid.jsonl{args.train_suffix}")
-    test_fname = os.path.join(DATA_PATH, f"test.jsonl{args.test_suffix}")
     print(colored(f"train_fname: {train_fname}", "yellow"))
     print(colored(f"valid_fname: {valid_fname}", "yellow"))
-    print(colored(f"test_fname: {test_fname}", "yellow"))
 
     num_epoch = args.num_epoch
     batch_size = args.batch_size
     learning_rate = args.learning_rate
     
     fr = get_dataset_distribution_name(args.train_suffix)
-    to = get_dataset_distribution_name(args.test_suffix)
-    run_id = f"{fr}_{to}"
-    if "token_mixture_preds" in args.train_suffix or "token_mixture_preds" in args.test_suffix:
+    run_id = f"{fr}"
+    if "token_mixture_preds" in args.train_suffix:
         run_id += f"_weight={args.token_mixture_multiplier}"
-
+        method_str = "-".join(args.method)
+        run_id += f"_method={method_str}"
     run_name = f"{run_id}_{num_epoch}_{batch_size}_{learning_rate}"
     print(colored(f"run_name: {run_name}", "cyan"))
     experiment_dir = f"./outputs/author_classification_100/{run_name}"
@@ -249,24 +290,34 @@ def main():
     
     model.load_state_dict(best_model)
     model.eval()
-    test_dataloader = get_dataloader(
-        os.path.join(DATA_PATH, test_fname),
-        batch_size=batch_size,
-        shuffle=False,
-    )
-    test_dataloader = accelerator.prepare(test_dataloader)
-    average_accuracy = 0
-    for batch in test_dataloader:
-        with torch.no_grad():
-            outputs = model(batch)
-            average_accuracy += outputs["accuracy"].item()
-    average_accuracy /= len(test_dataloader)
-    average_accuracy = gather_single_value(average_accuracy, accelerator)
+
+    suffixes = ["", ".mistral", ".mistral.mixed"]
+    if "token_mixture_preds" in args.train_suffix:
+        suffixes = [suffix + ".token_mixture_preds" for suffix in suffixes]
+    for suffix in suffixes:
+        test_fname = os.path.join(DATA_PATH, f"test.jsonl{suffix}")
+        print(colored(f"test_fname: {test_fname}", "yellow"))
+        test_dataloader = get_dataloader(
+            os.path.join(DATA_PATH, test_fname),
+            batch_size=batch_size,
+            shuffle=False,
+        )
+        test_dataloader = accelerator.prepare(test_dataloader)
+        average_accuracy = 0
+        for batch in test_dataloader:
+            with torch.no_grad():
+                outputs = model(batch)
+                average_accuracy += outputs["accuracy"].item()
+        average_accuracy /= len(test_dataloader)
+        average_accuracy = gather_single_value(average_accuracy, accelerator)
     
-    if accelerator.is_main_process:
-        print(colored(f"Average accuracy on test set: {average_accuracy}", "green"))
-    with open(os.path.join(experiment_dir, "results.json"), "w") as f:
-        json.dump({"test_accuracy": average_accuracy}, f, indent=4)
+        if accelerator.is_main_process:
+            print(colored(f"Average accuracy on test set: {average_accuracy}", "green"))
+
+        dist_name = get_dataset_distribution_name(test_fname)
+        save_fname = f"results_{dist_name}.json"
+        with open(os.path.join(experiment_dir, save_fname), "w") as f:
+            json.dump({"test_accuracy": average_accuracy}, f, indent=4)
             
     return 0
 
