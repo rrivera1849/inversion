a66ac20da883df1e6c87acae3eb5efa6cbc53b4e
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2024-10-10 17:13:17 -0700

    script to subsample right test authors

---
+++ scripts/dataset/style/subsample_test.py
@@ -0,0 +1,28 @@
+
+import json
+import os
+import sys
+
+import pandas as pd
+
+def get_test_authors():
+    path = "/data1/yubnub/changepoint/MUD_inverse/data/data.jsonl.filtered.cleaned_kmeans_100/inverse_output/test.small.jsonl"
+    authors = [json.loads(line)["author_id"] for line in open(path)]
+    return list(set(authors))
+
+def main():
+    base_path = "/data1/yubnub/changepoint/MUD_inverse/data"
+    dataset_name = sys.argv[1]
+    assert os.path.isdir(os.path.join(base_path, dataset_name)), f"Dataset {dataset_name} not found"
+
+    authors = get_test_authors()
+    df = pd.read_json(os.path.join(base_path, dataset_name, "test.jsonl"), lines=True)
+    df = df[df.author_id.isin(authors)]
+    os.makedirs(os.path.join(base_path, dataset_name, "inverse_output"), exist_ok=True)
+    df.to_json(os.path.join(base_path, dataset_name, "inverse_output", "test.small.jsonl"), lines=True, orient="records")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
